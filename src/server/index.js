bb75e49eaec29ad2abe65d0652aa9415ba3997a9
Kuba <jakubzengota@gmail.com>
2022-01-19 17:16:58 +0100

    css

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql')
+const mysql = require("mysql")
+const cors = require("cors")
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     user: "root",
@@ -15,18 +19,27 @@ app.post('/create', (req, res) => {
         const year = req.body.year
         const country = req.body.country
 
-        db.query('INSERT INTO films (name, type, year, country) VALUES (?,?,?,?)', 
+        db.query("INSERT INTO films (name, type, year, country) VALUES (?,?,?,?)", 
         [name, type, year, country],
         (err, result) => {
             if (err) {
-                console.log(err)
+                console.log(err);
             } else {
-                res.send("Values Inserted")
+                res.send("Values Inserted");
             }
         }
     );
 });
 
+app.get('/films', (req, res) => {
+    db.query("SELECT * FROM films", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
 
 app.listen(3001, () => {
     console.log("dupa is running on 3001");
