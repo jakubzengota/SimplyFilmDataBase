8a365967f6fbf5e78f7b12c14280e9c75689104b
Kuba <jakubzengota@gmail.com>
2022-01-19 18:34:18 +0100

    test project on docker

---
+++ PROJEKT_DO_DOCKERA/src/mysql/sql-scripts/CreateTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE films (
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` TEXT(255) NOT NULL,
+`type` TEXT(255) NOT NULL,
+`year` INT NOT NULL,
+`country` TEXT(255) NOT NULL,
+PRIMARY KEY (`id`)
+);
